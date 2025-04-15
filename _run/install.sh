fb998b44a6dc7cbc76233953c31daed4b4d78552
dorgon.chang <dorgonman@hotmail.com>
2025-04-15 10:21:38 +0800

    [Script] Add run script

---
+++ _run/install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e  
+pushd ..
+
+pip install virtualenv
+
+
+if [ ! -d ".venv" ]; then
+  /c/Python312/python -m virtualenv .venv
+fi
+
+VENV_PYTHON_PATH=$(pwd)/.venv/Scripts/python
+VENV_PIP_PATH=$(pwd)/.venv/Scripts/pip
+
+
+$VENV_PYTHON_PATH --version
+$VENV_PYTHON_PATH -m pip install --upgrade pip
+
+$VENV_PIP_PATH install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu126
+$VENV_PIP_PATH install -r requirements.txt
+
+$VENV_PIP_PATH install $(pwd)/_run/addon/insightface-0.7.3-cp312-cp312-win_amd64.whl
+popd
+
+
+
+
+pushd .. > /dev/null
+projectRoot=$(pwd)
+for i in $(find "custom_nodes" -type d -name '.git' -prune)
+do
+    repo=$(echo ${i%.git*})
+    pushd $repo > /dev/null
+       # check if requirements.txt exists
+       if [ -f "requirements.txt" ]; then
+           echo "Installing requirements for $repo"
+           cmd="$VENV_PIP_PATH install -r ${projectRoot}/$repo/requirements.txt"
+           echo $cmd
+           eval $cmd
+       fi    
+
+    popd > /dev/null
+done
+popd > /dev/null
