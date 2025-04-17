7800716b026236c8f41be8949d942efd26e1b071
dorgon.chang <dorgonman@hotmail.com>
2025-04-17 21:52:07 +0800

    [Run] implement install requirements.txt for plugins

--- _run/install.sh
+++ _run/install.sh
@@ -24,3 +24,24 @@ $VENV_PYTHON_PATH -m pip install --upgrade pip
 $VENV_PIP_PATH install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu126
 $VENV_PIP_PATH install -r requirements.txt
 popd
+
+
+
+
+pushd .. > /dev/null
+projectRoot=$(pwd)
+for i in $(find "custom_nodes" -type d -name '.git' -prune)
+do
+    repo=$(echo ${i%.git*})
+    pushd $repo > /dev/null
+       # check if requirements.txt exists
+       if [ -f "requirements.txt" ]; then
+           echo "Installing requirements for $repo"
+           cmd="$VENV_PIP_PATH install -r ${projectRoot}/$repo/requirements.txt"
+           echo $cmd
+           eval $cmd
+       fi    
+
+    popd > /dev/null
+done
+popd > /dev/null
